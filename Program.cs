99bb6c5834e1da3af1aeba2c56876dc6c9dac920
pedrincsharp <joao_pedro_alves_moraes@outlook.com.br>
2024-09-05 12:32:19 -0300

    Completando o desafio

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ Pessoa p2 = new Pessoa(nome: "Hóspede 2");
 hospedes.Add(p1);
 hospedes.Add(p2);
 
+
 // Cria a suíte
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
